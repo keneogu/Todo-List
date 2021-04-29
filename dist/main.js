7c035129aca7a8dc26bd44caa4fa1e0265cc32f6
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-04-29 16:56:21 +0100

    Added and test webpack

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("#content"),t=document.createElement("h1");t.textContent="John is a nice guy",e.appendChild(t)})();
+//# sourceMappingURL=main.js.map
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const john = document.querySelector('#content');
+const head = document.createElement('h1');
+head.textContent = 'John is a nice guy';
+john.appendChild(head);
+
