555ed236ddeac32c498ce7409326a9fe70bca0b4
udberg <johnuduak066@gmail.com>
2021-05-03 11:40:08 +0100

    Create new project directory and initialize conditional logic for already existing and new ones

--- src/project.js
+++ src/project.js
@@ -8,3 +8,24 @@ class Project {
     this.todos = [];
   }
 }
+
+const projectModule = (() => {
+  function createProject(target) {
+    const projects = getProjects();
+    const targetValue = target.value.toLowerCase();
+    const invalidName = projects.find((element) => element.name.toLowerCase() === targetValue);
+
+    if (target.value !== '' && !invalidName) {
+      const newProject = new Project(target.value);
+      projects.push(newProject);
+      localStorage.toDoProjects = JSON.stringify(projects);
+
+      start();
+    } else {
+      alert('Project name already exist or empty');
+    }
+    target.value = '';
+  }
+
+  return { createProject };
+})();
