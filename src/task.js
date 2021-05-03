cd3ea38a876094bad886b0da390159b800bd3d95
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-05-03 09:44:39 +0100

    Add todo class constructor properties

---
+++ src/task.js
@@ -0,0 +1,19 @@
+class Todo {
+	constructor(title, date, description, priority, project) {
+	  this.title = title;
+	  this.date = date;
+	  this.description = description;
+	  this.priority = priority;
+	  this.project = project;
+	  this.id = Date.now().toString();
+	  this.status = false;
+	}
+  
+	updateTodo(title, date, description, priority) {
+	  this.title = title;
+	  this.date = date;
+	  this.description = description;
+	  this.priority = priority;
+	  return this;
+	}
+  }
