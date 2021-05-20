f30941422cf094a3387f0ee46c9c1e830c0ed6a9
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-05-20 15:01:23 +0100

    modified project test

--- test/project.test.js
+++ test/project.test.js
@@ -1,20 +1,20 @@
-const initStart = require('../src/initStart');
-const project = require('../src/project');
+const start = require("../src/start");
+const project = require("../src/project");
 
-const target = document.createElement('input');
-target.setAttribute('type', 'text');
-target.value = 'Jest-test';
-const projects = initStart.getProjects();
+const target = document.createElement("input");
+target.setAttribute("type", "text");
+target.value = "Jest-test";
+const projects = start.getProjects();
 
-describe('createProject', () => {
-  it('Creates a new Project', () => {
-    const newProject = project.projectModule.createProject(target);
-    expect(newProject instanceof project.Project).toEqual(true);
+describe("createProject", () => {
+  it("Creates a new Project", () => {
+    const newProject = projects.projectModule.createProject(target);
+    expect(newProject).toEqual('Jest-test');
   });
 });
 
-describe('removeProject', () => {
-  it('Removes a new Project', () => {
+describe("removeProject", () => {
+  it("Removes a new Project", () => {
     const resultProjects = project.removeProject(projects, projects[0]);
     expect(resultProjects.length).toBe(projects.length - 1);
   });
